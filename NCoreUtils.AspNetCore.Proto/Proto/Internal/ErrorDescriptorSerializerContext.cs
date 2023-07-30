0bd97498dc91e1cb5a5ce9e360154e96635a2a70
Artyom Antyipin <artyom.szasa@gmail.com>
2023-07-30 10:06:43 +0200

    move error serialization to shared library (avoids name collision when using both client ans service generation).

--- NCoreUtils.AspNetCore.Proto/Proto/Internal/ErrorDescriptorSerializerContext.cs
+++
@@ -1,7 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace NCoreUtils.Proto.Internal;
-
-[JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
-[JsonSerializable(typeof(ErrorDescriptor))]
-public partial class ErrorDescriptorSerializerContext : JsonSerializerContext { }
