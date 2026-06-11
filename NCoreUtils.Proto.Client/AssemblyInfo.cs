bce395fff6d30a1a1a9d50277e8452debd32f1c5
Artyom Antyipin <artyom.szasa@gmail.com>
2026-06-11 09:18:01 +0200

    type forwarding

---
+++ NCoreUtils.Proto.Client/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+using NCoreUtils.Proto.Internal;
+
+[assembly: TypeForwardedTo(typeof(ProtoJsonContent))]
+[assembly: TypeForwardedTo(typeof(ProtoJsonContent<>))]
